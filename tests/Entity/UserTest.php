2dd5cd6623223d070f0189b337dc5581a809f403
elaneg <elane.grd@gmail.com>
2024-04-17 14:31:44 +0200

    feature/testsUnitaires : tests des getters et setters des 3 entités

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -2,25 +2,39 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Subscription;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testGetterAndSetter()
+    public function testGetterAndSetterUser()
     {
         // Création d'une instance de l'entité User
         $user = new User();
 
         // Définition de données de test
         $email = 'test@test.com';
-        //[.. ICI VOS AUTRES SETTERS ..]
+        $firstname = 'firstname';
+        $lastname = 'lastname';
+        $password = 'password';
+        $role = 'role';
+        $sub_id = new Subscription();
 
         // Utilisation des setters
-        $user->setEmail($email);
+        $user->setEmail($email)
+            ->setFirstname($firstname)
+            ->setLastname($lastname)
+            ->setPassword($password)
+            ->setRole($role)
+            ->setSubscriptionId($sub_id);
 
         // Vérification des getters
         $this->assertEquals($email, $user->getEmail());
-       // [.. ETC ..]
+        $this->assertEquals($firstname, $user->getFirstname());
+        $this->assertEquals($lastname, $user->getLastname());
+        $this->assertEquals($password, $user->getPassword());
+        $this->assertEquals($role, $user->getRole());
+        $this->assertEquals($sub_id, $user->getSubscriptionId());
     }
 }
