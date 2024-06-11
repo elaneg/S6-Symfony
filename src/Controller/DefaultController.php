2438e20d2feec6e1614e9a094104d64c05cc5325
elaneg <elane.grd@gmail.com>
2024-06-11 16:42:16 +0200

    feature/pages : mise en place de toutes les pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
+/**
+* @Route("/", name="homepage")
+*/
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
