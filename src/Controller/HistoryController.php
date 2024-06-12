12d75bdfc27243765572fa20b26a8afa72da70ff
elaneg <elane.grd@gmail.com>
2024-06-12 11:37:22 +0200

    fixture/corrections : corrections

--- src/Controller/HistoryController.php
+++ src/Controller/HistoryController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use App\Repository\PdfRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\History;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HistoryController extends AbstractController
 {
-
     private $PdfRepository;
 
     public function __construct(PdfRepository $PdfRepository)
